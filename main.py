a9d2d3e3c8041ef64c02b86f7671c4a7bba1f93c
stanly-johnson <stanlyjohnson01@gmail.com>
2015-08-26 22:40:23 +0530

    main-edit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 #Live human detection robot
-
+#Main file for beaglebone black
+#to be loaded on beaglebone -- assuming webcam attached to usb
 
 print "Hello World"
 print "Space-time analysis"
+print "Live human detection robot"
+print "cynosure 2015"
